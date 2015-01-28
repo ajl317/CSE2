f9078d6b9688f069750bda5ed3459fa148971a42
ajl317 <ajl317@lehigh.edu>
2015-01-28 22:27:25 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,29 @@
+//Tony Lonero
+//hw 01
+//Welcome Class Java Program
+//
+//  first compile the program
+//      javac HelloWorld.java
+//  run the program
+//      java HelloWorld//
+
+//  define a class
+public class WelcomeClass{
+    
+//  add main method
+   public static void main(String[] args) {
+       
+//  print the welcome message with a short bio at the bottom
+    System.out.println("  -------------");
+    System.out.println("  |  WELCOME  |");
+    System.out.println("  -------------");
+    System.out.println("  ^  ^  ^  ^  ^  ^");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+    System.out.println("<-A--J--L--3--1--7->");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+    System.out.println("  v  v  v  v  v  v");
+    System.out.println("I am a sophomore in the IBE program but I do not know how to code in Java at all.");
+    
+    //test/testtest
+   }
+}
